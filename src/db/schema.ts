4ceb30148323e17c189ba3b1749ec53b747fc283
focusgabriel <focusgabriel@users.noreply.github.com>
2026-07-08 01:06:24 +0100

    changed some keywords in schema.ts

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -7,7 +7,7 @@
 // 
 
 import {pgTable, serial, text, timestamp, numeric, integer} from "drizzle-orm/pg-core";
-export const users = pgTable("users", {
+export const user = pgTable("user", {
   id: serial("id").primaryKey(),
   email: text("email").notNull().unique(),
   createdAt: timestamp("created_at").defaultNow().notNull(),
@@ -30,7 +30,7 @@ export const orders = pgTable('orders', {
   id: serial("id").primaryKey(),
 
   // Foreign Key referencing the users table - ensures the order belongs to a valid user
-  userId: text("name").notNull().references(() => users.id).notNull(),
+  userId: integer("user_id").notNull().references(() => user.id).notNull(),
 
   // Total order amount stored as a numeric value for precise calculation.
   totalAmount: numeric("total_amount").notNull(),
@@ -58,4 +58,4 @@ export const orderItems = pgTable("order_items", {
   // The price of the product at the time of purchase (snapshot, since prices may change later)
   priceAtPurchase: numeric("price_at_purchase").notNull(), 
 
-})
+});
