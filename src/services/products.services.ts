6af386811f52c8c94188276e9602d60b61faeb1f
focusgabriel <focusgabriel@users.noreply.github.com>
2026-07-08 15:21:56 +0100

    added the services file for creating a product

---
+++ src/services/products.services.ts
@@ -0,0 +1,14 @@
+import { db } from "../db";
+import { products } from "../db/schema";
+
+// =============== CREATE PRODUCT ==================
+// Inserts a new products with the given name, price, and optional stock quantity
+// stockQuantity defaults to 0 if not provided
+// Returns the newly created product object
+
+export async function CreateProduct(name:string, currentPrice: string, stockQuantity:number = 0) {
+  // Insert INTO products (name, current_price, stock_quantity) Values (?, ?, ?)RETURNING *
+  const result = await db.insert(products).values({name, currentPrice, stockQuantity}).returning();
+  // Return the inserted row, or null if insertion returned nothing 
+  return result[0] ?? null;
+}
