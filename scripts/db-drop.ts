7852f0bda8350491bc47c60e7ac419fc382d442c
focusgabriel <focusgabriel@users.noreply.github.com>
2026-07-14 16:54:50 +0100

    implemented a dd-drop file to delete all data in db

---
+++ scripts/db-drop.ts
@@ -0,0 +1,41 @@
+import "dotenv/config";
+import { Client } from "pg";
+
+async function dropAllTables() {
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+  });
+
+  try{
+  await client.connect();
+  //  find all tables in the public schema
+
+  const { rows } = await client.query(`
+    SELECT tablename
+    FROM pg_tables
+    WHERE schemaname = "public";
+   `);
+  if(rows.length === 0){
+    console.log("No tables found");
+    return;
+  }
+
+  // Drop every tables
+  for(const {tablename} of rows) {
+    console.log(`Dropping table: ${tablename}`)
+  
+  await client.query(
+    `DROP TABLES IF EXISTS "${tablename}" CASCADE;`
+  );
+}
+console.log("All tables dropped successfully");
+
+} catch(error) {
+  console.error(" Failed to drop tables", error);
+  process.exit(1);
+} finally {
+  await client.end();
+}
+}
+
+dropAllTables();
